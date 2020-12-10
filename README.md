01f3bbdfb3fb3c4ab8733a9e4036876e1cf3cd83
knyL <knyl@knyLs-iMac-Pro.local>
2020-12-10 22:25:55 +0700

    update injector get for two-ways passing data

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
     @angular/cdk/drag-drop
 
 ## Notes
+- Using Injector get | for 'two-ways passing data': Input | Inject
 - Pass data to Modal Type using ComponentPortal.
 - Returm data back.
 - Drag Modal Overlay with `cdkDragRootElement`.
--- modal-overlay-app/src/app/components/msg/msg.component.ts
+++ modal-overlay-app/src/app/components/msg/msg.component.ts
@@ -1,5 +1,5 @@
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, Injector, OnInit } from '@angular/core';
 import { ModalOverlayRef } from 'src/app/modal-overlay/modal-overlay-ref';
 
 @Component({
@@ -7,11 +7,24 @@ import { ModalOverlayRef } from 'src/app/modal-overlay/modal-overlay-ref';
   templateUrl: './msg.component.html',
 })
 export class MsgComponent implements OnInit {
+  componentData;
   msg: string;
   arr;
-  constructor(@Inject(ModalOverlayRef) private componentData: any) {
-    this.msg = componentData.data.msg;
-    this.arr = componentData.data.arr;
+  // constructor(@Inject(ModalOverlayRef) private componentData: any) {
+  //   this.msg = componentData.data.msg;
+  //   this.arr = componentData.data.arr;
+  // }
+
+  // get injector | for two-ways binding (Input & inject)
+  constructor(private injector: Injector) {
+    try{
+      this.componentData= injector.get(ModalOverlayRef);
+      if (this.componentData.content=== MsgComponent){
+        this.msg = this.componentData.data.msg;
+        this.arr = this.componentData.data.arr;
+      }
+    }
+   catch {}
   }
 
   ngOnInit(): void {}
